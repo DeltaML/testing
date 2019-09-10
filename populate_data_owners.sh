c72af560d0ccc3fe65ad126c4bd0f6c7af268d5f
Agustin <rojasagustin90@gmail.com>
2019-09-10 15:08:09 -0300

    Update populate_data_owners.sh

--- populate_data_owners.sh
+++ populate_data_owners.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-curl -v -X POST -F file=@"dataset/file_1.csv" http://localhost:5000/datasets & \
-curl -v -X POST -F file=@"dataset/file_1.csv" http://localhost:5001/datasets & \
-curl -v -X POST -F file=@"dataset/file_1.csv" http://localhost:5002/datasets & \
-curl -v -X POST -F file=@"dataset/file_1.csv" http://localhost:5003/datasets & \
-curl -v -X POST -F file=@"dataset/file_1.csv" http://localhost:5004/datasets 
+curl -v -X POST -F file=@"datasets/file.csv" http://localhost:5000/datasets & \
+curl -v -X POST -F file=@"datasets/file.csv" http://localhost:5001/datasets & \
+curl -v -X POST -F file=@"datasets/file.csv" http://localhost:5002/datasets & \
+curl -v -X POST -F file=@"datasets/file.csv" http://localhost:5003/datasets & \
+curl -v -X POST -F file=@"datasets/file.csv" http://localhost:5004/datasets 
